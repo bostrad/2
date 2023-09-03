b3af0dd92b1f43bc2dfe6b1e333e294a1f22ef00
bostrad <143945475+bostrad@users.noreply.github.com>
2023-09-03 20:48:31 +0700

    1.js

---
+++ 1.js
@@ -0,0 +1,7 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){	 
+	(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://pl20515955.highcpmrevenuegate.com/7e/35/eb/7e35ebcbcaa7b4438a2b50a929c03a78.js',document.body||document.documentElement);
+	 }
