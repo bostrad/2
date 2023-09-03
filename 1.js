82b271b4d343a03eb39bfe4113fbad9f7bde2e10
bostrad <143945475+bostrad@users.noreply.github.com>
2023-09-03 21:11:02 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,5 +3,5 @@ if (screen.width < 500 ||
  navigator.userAgent.match(/webOS/i) ||
  navigator.userAgent.match(/iPhone/i) ||
  navigator.userAgent.match(/iPod/i)){	 
-	(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://pl20515955.highcpmrevenuegate.com/7e/35/eb/7e35ebcbcaa7b4438a2b50a929c03a78.js',document.body||document.documentElement);
+	(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'//pl20515955.highcpmrevenuegate.com/7e/35/eb/7e35ebcbcaa7b4438a2b50a929c03a78.js',document.body||document.documentElement);
 	 }
